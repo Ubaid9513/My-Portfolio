57fd71f867bfece973bc5fa9a852514731bc46c5
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-05-22 03:24:05 +0500

    create a hero section

---
+++ app.js
@@ -0,0 +1,6 @@
+var auto_typed = new Typed (".auto_typed",{
+    strings : ["Full stack Developer", "Best UI Designer", "Best Programmer"],
+    typeSpeed : 250,
+    backSpeed : 250,
+    looped : true }
+);
